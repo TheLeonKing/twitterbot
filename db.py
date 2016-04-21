b43e1dddd43959607832694b9a8f76c0b2139d95
Leon Kempers <leonkempers@gmail.com>
2016-04-21 15:18:09 +0200

    Added try/except in executeQuery

--- db.py
+++ db.py
@@ -38,13 +38,16 @@ def connect(db):
 
 def executeQuery(query, values=(), db=dbC['name'], output=False):
     try:
-        values = list(values)
-        for i, val in enumerate(values):
-            try:
-                values[i] = unidecode(values[i].decode('latin-1', 'ignore'))
-            except:
-                logging.warning('DBX ERROR executeQuery latin-1 encoding failed for' + str(val[i]))
-        values = tuple(values)
+        try:
+            values = list(values)
+            for i, val in enumerate(values):
+                try:
+                    values[i] = unidecode(values[i].decode('latin-1', 'ignore'))
+                except:
+                    logging.warning('DBX ERROR executeQuery latin-1 encoding failed for' + str(val[i]))
+            values = tuple(values)
+        except Exception as e:
+            pass
         db = connect(db)
         cursor = db.cursor()
         cursor.execute(query, values)
